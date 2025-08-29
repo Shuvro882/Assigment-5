acf4e9168154f6963bbd454e9b594f4b74081e8f
Shuvro882 <sahashuvro74@gmail.com>
2025-08-29 13:30:26 +0600

    added some js and added some problem solving

---
+++ emergency.js
@@ -0,0 +1,49 @@
+
+//Heart click
+const heartCountElement = document.getElementById('navbar-heart-count'); 
+    
+    let heartCount = parseInt(heartCountElement.innerText); 
+
+   
+    const cardHearts = document.getElementsByClassName('card-heart'); 
+    
+    for (let i = 0; i < cardHearts.length; i++) {
+        cardHearts[i].addEventListener('click', function() {
+            heartCount++; 
+            heartCountElement.innerText = heartCount; 
+        });
+    }
+
+//Call click
+
+const coinElement = document.getElementById('navbar-coin-count');
+let coins = parseInt(coinElement.innerText);
+
+const callHistoryList = document.getElementById('call-history-list');
+
+const callButtons = document.getElementsByClassName('call-btn');
+
+for (let i = 0; i < callButtons.length; i++) {
+    callButtons[i].addEventListener('click', function() {
+        const id = this.getAttribute('data-id');
+        const serviceName = document.getElementById(`service-name-${id}`).innerText;
+        const serviceNumber = document.getElementById(`service-number-${id}`).innerText;
+
+        if (coins < 20) {
+            alert("Not enough coins to make a call!");
+            return;
+        }
+
+        coins -= 20;
+        coinElement.innerText = coins;
+
+        alert(`Calling ${serviceName}: ${serviceNumber}`);
+
+        const historyItem = document.createElement('div');
+        historyItem.className = "bg-gray-100 p-2 my-1 rounded";
+        historyItem.innerHTML = `<strong>${serviceName}</strong> - ${serviceNumber}`;
+        callHistoryList.appendChild(historyItem);
+    });
+}
+
+
